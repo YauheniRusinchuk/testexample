8ba8107fe5bda1f33398cb17eef946dd63a3179f
Русинчук Евгений <rusinchuk@bpcbt.com>
2024-04-03 18:20:18 +0300

    init

---
+++ assets/Loader.5e92401e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,h as t,t as a}from"./gpb-new.3d7b750a.js";const s={},c={class:"loader"};function n(o,_){return r(),t("div",c,a(o.$t("loading")),1)}var l=e(s,[["render",n]]);export{l as default};
